77c5f58ccdadd5e4d30ffc494de7b58fbe58a8de
Swapnil Mirjolkar <swapnilm@maqsoftware.com>
2016-10-28 15:23:22 +0530

    Commited the Consumer Project and Submodule of the libraries

---
+++ MessageConsumer/MessageConsumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Here goes the test code
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
